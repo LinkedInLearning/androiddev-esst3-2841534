ddbb0759c5c18782964110f20d369d3fc3445528
Annyce <annyce.davis@gmail.com>
2020-10-15 13:42:32 -0400

    [+] Added a MutableLiveData

--- app/src/main/java/com/example/myapplication/MainViewModel.kt
+++ app/src/main/java/com/example/myapplication/MainViewModel.kt
@@ -1,17 +1,22 @@
 package com.example.myapplication
 
 import android.util.Log
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 private const val LOG_TAG = "MainViewModel"
 
 class MainViewModel : ViewModel() {
 
+    private val _info : MutableLiveData<Int> = MutableLiveData()
+
     init {
         Log.i(LOG_TAG, "created")
+        _info.value = 0
     }
 
     fun loadData() {
-        Log.i(LOG_TAG, "load data")
+        _info.value = _info.value!! + 1
+        Log.i(LOG_TAG, "current value: ${_info.value}")
     }
 }
