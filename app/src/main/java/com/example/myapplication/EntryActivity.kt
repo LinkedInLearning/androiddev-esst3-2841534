187af84c338353d6892cc08dcc896413a2740724
Annyce <annyce.davis@gmail.com>
2020-10-31 14:58:40 -0400

    [+] Created explicit intent to navigate to MainActivity

--- app/src/main/java/com/example/myapplication/EntryActivity.kt
+++ app/src/main/java/com/example/myapplication/EntryActivity.kt
@@ -1,11 +1,22 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.myapplication.databinding.ActivityEntryBinding
 
 class EntryActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityEntryBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_entry)
+        binding = ActivityEntryBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.enterButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
