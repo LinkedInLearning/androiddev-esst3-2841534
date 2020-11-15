92d332bbda33fff799d5f7816e6ea261e1524ada
Annyce <annyce.davis@gmail.com>
2020-11-15 13:06:20 -0500

    [+] Added an implicit share intent

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myapplication.databinding.ActivityMainBinding
@@ -11,6 +13,8 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private val viewModel by viewModels<MainViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -18,8 +22,6 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.toolbar)
         binding.toolbarLayout.title = getString(R.string.app_name)
-
-        val viewModel by viewModels<MainViewModel>()
 
         viewModel.info.observe(this) {
             displaySnackbar(it)
@@ -40,5 +42,23 @@ class MainActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_main, menu)
         return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId) {
+            R.id.action_share -> handleShare()
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun handleShare(): Boolean {
+        val intent = Intent().apply {
+            action = Intent.ACTION_SEND
+            type = "text/plain"
+            putExtra(Intent.EXTRA_TEXT,
+                    "I just purchased ${viewModel.info.value} bottles of olive oil!")
+        }
+        startActivity(intent)
+        return true
     }
 }
