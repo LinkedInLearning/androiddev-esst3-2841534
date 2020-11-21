0fdbc94ad0e69bd6ce3251fefdbc1de8e851e1f2
Annyce <annyce.davis@gmail.com>
2020-11-21 16:53:08 -0500

    [*] Prepped for the use of NavigationView

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,64 +1,25 @@
 package com.example.myapplication
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
-import androidx.activity.viewModels
-import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
 import com.example.myapplication.databinding.ActivityMainBinding
-import com.google.android.material.snackbar.Snackbar
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : FragmentActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
-    private val viewModel by viewModels<MainViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setSupportActionBar(binding.toolbar)
-        binding.toolbarLayout.title = getString(R.string.app_name)
+        if (savedInstanceState != null) return
 
-        viewModel.info.observe(this) {
-            displaySnackbar(it)
-        }
-        binding.fab.setOnClickListener {
-            viewModel.loadData()
+        supportFragmentManager.commit {
+            add<HomeFragment>(R.id.container, null)
         }
     }
 
-    private fun displaySnackbar(count: Int) {
-        Snackbar.make(
-            binding.root,
-            "Current value: $count",
-            Snackbar.LENGTH_LONG
-        ).show()
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return super.onCreateOptionsMenu(menu)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when(item.itemId) {
-            R.id.action_share -> handleShare()
-            else -> return super.onOptionsItemSelected(item)
-        }
-    }
-
-    private fun handleShare(): Boolean {
-        val intent = Intent().apply {
-            action = Intent.ACTION_SEND
-            type = "text/plain"
-            putExtra(Intent.EXTRA_TEXT,
-                    "I just purchased ${viewModel.info.value} bottles of olive oil!")
-        }
-        startActivity(intent)
-        return true
-    }
 }
---
+++ app/src/main/java/com/example/myapplication/ShopFragment.kt
@@ -0,0 +1,28 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.myapplication.databinding.FragmentShopBinding
+
+class ShopFragment : Fragment() {
+
+    private var _binding: FragmentShopBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentShopBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
